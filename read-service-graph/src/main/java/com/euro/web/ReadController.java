8b87a536e3cf8b02d76760dc828d616c4f043bce
SASIKUMAR SUGUMAR <sasisugumar@gmail.com>
2016-08-30 22:21:50 -0400

    Initial Import

--- read-service-graph/src/main/java/com/euro/web/ReadController.java
+++ read-service-graph/src/main/java/com/euro/web/ReadController.java
@@ -3,10 +3,22 @@
  */
 package com.euro.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author euro
  *
  */
+@RestController
 public class ReadController {
 
+	
+	@RequestMapping(value="/getAllNodes",method=RequestMethod.GET)
+	public @ResponseBody String getAllNodes(){
+		return "ALL Nodes";
+	}
+	
 }
--- write-service-graph/src/main/java/com/euro/web/WriteController.java
+++ write-service-graph/src/main/java/com/euro/web/WriteController.java
@@ -3,10 +3,21 @@
  */
 package com.euro.web;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author euro
  *
  */
+@RestController
 public class WriteController {
 
+	@RequestMapping(value="/createNode",method=RequestMethod.GET)
+	public @ResponseBody String createNode(){
+		return "ALL Nodes";
+	}
+	
 }
